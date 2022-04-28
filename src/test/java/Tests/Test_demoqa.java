3be654816ef4ddba3826790852a42bf8586d72f4
sebastian-ramos1996 <sebasramos1996@gmail.com>
2022-04-28 14:00:14 -0500

    actualización Test_demoqa.java

--- src/test/java/Tests/Test_demoqa.java
+++ src/test/java/Tests/Test_demoqa.java
@@ -2,9 +2,9 @@ package Tests;
 
 import Baseclass.BaseClass;
 import Helpers.Helpers;
+import Pages.PageElementsDynamicsProperties;
 import Pages.PageForms;
 import Pages.PageIndex;
-import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class Test_demoqa extends BaseClass {
@@ -15,8 +15,6 @@ public class Test_demoqa extends BaseClass {
         Helpers helpers = new Helpers(driver);
         helpers.getURL(helpers.getXMLParameter("url"));
         helpers.scrollDown();
-        //JavascriptExecutor js = (JavascriptExecutor) driver;
-        //js.executeScript("window.scrollTo(0,500)");
 
         PageIndex pageIndex = new PageIndex(driver);
         pageIndex.click_card_Forms();
@@ -24,8 +22,24 @@ public class Test_demoqa extends BaseClass {
         PageForms pageForms = new PageForms(driver);
         pageForms.click_practice_Form_button();
         pageForms.fill_form_practice_Form();
-        pageForms.click_submit_button();
+        pageForms.press_submit_button();
+        pageForms.click_close_button();
 
+
+    }
+
+   // @Test
+    public void Test_DynamicsProperties(){
+        Helpers helpers = new Helpers(driver);
+        helpers.getURL(helpers.getXMLParameter("url"));
+        helpers.scrollDown();
+
+        PageIndex pageIndex = new PageIndex(driver);
+        pageIndex.click_card_Elements();
+        helpers.scrollDown();
+        helpers.scrollDown();
+        PageElementsDynamicsProperties pageElementsDynamicsProperties = new PageElementsDynamicsProperties(driver);
+        pageElementsDynamicsProperties.click_DynamicsProperties_button();
 
     }
 }
