99959523e083c42860ca3dac549f216b8adbd4c7
HaifischCQBO <victor.briones07@gmail.com>
2022-04-29 10:25:37 -0400

    Correcciones en Pruebas

--- src/main/java/pages/demoqa/Page_Elements_Links.java
+++ src/main/java/pages/demoqa/Page_Elements_Links.java
@@ -23,19 +23,13 @@ public class Page_Elements_Links {
     public By select_link_home = By.id("simpleLink");
     public By Select_link_homeFWDZo = By.id("dynamicLink");
     public By Select_Link_created = By.id("created");
-    public By message_created = By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/p[10]");
+    public By api_response = By.id("linkResponse");
     public By Select_Link_nocontent = By.id("no-content");
-    public By message_nocontent = By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/p[10]");
     public By Select_Link_moved = By.id("moved");
-    public By message_moved = By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/p[10]");
     public By Select_Link_badrequest = By.id("bad-request");
-    public By message_badrequest = By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/p[10]");
     public By Select_Link_unauthorized = By.id("unauthorized");
-    public By message_unauthorized = By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/p[10]");
     public By Select_Link_forbidden = By.id("forbidden");
-    public By message_forbidden = By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/p[10]");
     public By Select_Link_notfound = By.id("invalid-url");
-    public By message_notfound = By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[2]/p[10]");
 
 
 
@@ -63,49 +57,49 @@ public class Page_Elements_Links {
 
     }
     public String  message_confirmation_created(){
-        return helpers.getText(message_created);
+        return helpers.getText(api_response);
     }
     public void click_link_nocontent(){
         helpers.clickBy(Select_Link_nocontent);
 
     }
     public String  message_confirmation_nocontent(){
-        return helpers.getText(message_nocontent);
+        return helpers.getText(api_response);
     }
     public void click_link_moved(){
         helpers.clickBy(Select_Link_moved);
 
     }
     public String  message_confirmation_moved(){
-        return helpers.getText(message_moved);
+        return helpers.getText(api_response);
     }
     public void click_link_badrequest(){
         helpers.clickBy(Select_Link_badrequest);
 
     }
     public String  message_confirmation_badrequest(){
-        return helpers.getText(message_badrequest);
+        return helpers.getText(api_response);
     }
     public void click_link_unauthorized(){
         helpers.clickBy(Select_Link_unauthorized);
 
     }
     public String  message_confirmation_unauthorized(){
-        return helpers.getText(message_unauthorized);
+        return helpers.getText(api_response);
     }
     public void click_link_forbidden(){
         helpers.clickBy(Select_Link_forbidden);
 
     }
     public String  message_confirmation_forbidden(){
-        return helpers.getText(message_forbidden);
+        return helpers.getText(api_response);
     }
     public void click_link_notfound(){
         helpers.clickBy(Select_Link_notfound);
 
     }
     public String  message_confirmation_notfound(){
-        return helpers.getText(message_notfound);
+        return helpers.getText(api_response);
     }
 
 
--- src/test/java/Tests/Test_con_POM.java
+++ src/test/java/Tests/Test_con_POM.java
@@ -29,47 +29,42 @@ public class Test_con_POM extends BaseClass {
         Page_Elements page_elements = new Page_Elements(driver);
         page_elements.click_element();
 
-        //Pagina Page_Elements_Links
+        //Pagina Page_Elements_Links        Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
+        Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
+
+
         // Link Home
-        /*Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
-        page_elements_links.click_link_home();*/
+        //page_elements_links.click_link_home();
 
         // Link HomeFWDZo
-        /*Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
-        page_elements_links.click_link_homefwdzo();*/
+        //page_elements_links.click_link_homefwdzo();
 
-        // Link Created
-        /*Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
+        // Lin*k Created
         page_elements_links.click_link_created();
         helpers.Pause(5);
-        Assert.assertEquals(page_elements_links.message_confirmation_created(),"Link has responded with staus 201 and status text Created");*/
+        Assert.assertEquals(page_elements_links.message_confirmation_created(),"Link has responded with staus 201 and status text Created");
 
         //Link No Content
-        /*Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
         page_elements_links.click_link_nocontent();
         helpers.Pause(4);
-        Assert.assertEquals(page_elements_links.message_confirmation_nocontent(),"Link has responded with staus 204 and status text No Content");*/
+        Assert.assertEquals(page_elements_links.message_confirmation_nocontent(),"Link has responded with staus 204 and status text No Content");
 
         // Link Moved
-        /*Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
         page_elements_links.click_link_moved();
         helpers.Pause(4);
-        Assert.assertEquals(page_elements_links.message_confirmation_moved(),"Link has responded with staus 301 and status text Moved Permanently");*/
+        Assert.assertEquals(page_elements_links.message_confirmation_moved(),"Link has responded with staus 301 and status text Moved Permanently");
 
         // Link Bad Request
-        /*Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
         page_elements_links.click_link_badrequest();
         helpers.Pause(4);
-        Assert.assertEquals(page_elements_links.message_confirmation_badrequest(),"Link has responded with staus 400 and status text Bad Request");*/
+        Assert.assertEquals(page_elements_links.message_confirmation_badrequest(),"Link has responded with staus 400 and status text Bad Request");
 
         // Link Bad Request
-        /*Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
         page_elements_links.click_link_forbidden();
         helpers.Pause(5);
-        Assert.assertEquals(page_elements_links.message_confirmation_forbidden(),"Link has responded with staus 403 and status text Forbidden");*/
+        Assert.assertEquals(page_elements_links.message_confirmation_forbidden(),"Link has responded with staus 403 and status text Forbidden");
 
         // Link Bad Request
-        Page_Elements_Links page_elements_links = new Page_Elements_Links(driver);
         page_elements_links.click_link_notfound();
         helpers.Pause(4);
         Assert.assertEquals(page_elements_links.message_confirmation_notfound(),"Link has responded with staus 404 and status text Not Found");
