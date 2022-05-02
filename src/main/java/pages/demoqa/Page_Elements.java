69d3b7e628fc0ceec7111d5e29c5c8b2856dfe73
Victor Briones <victor.briones07@gmail.com>
2022-05-01 23:48:13 -0400

    se corrigieron errores relacionados

--- src/main/java/pages/demoqa/Page_Elements.java
+++ src/main/java/pages/demoqa/Page_Elements.java
@@ -1,37 +1,3 @@
-package pages.demoqa;
-
-import Helpers.Helpers;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-
-public class Page_Elements {
-
-    public WebDriver driver;
-    public Helpers helpers;
-
-    public Page_Elements(WebDriver driver){
-        this.driver = driver;
-        helpers = new Helpers(driver);
-    }
-
-    /**
-     * ---------------------------------------------------------------------------------------------------------
-     *  WebElements // WebElements // WebElements // WebElements // WebElements // WebElements // WebElements //
-     * ---------------------------------------------------------------------------------------------------------
-     */
-    public By select_element = By.xpath("//*[@id=\"item-5\"]");
-
-
-
-    /**
-     * -----------------------------------------------------------------------------------------------------------
-     *  Funciones // Funciones //  Funciones //  Funciones //  Funciones //  Funciones //  Funciones //  Funciones
-     * -----------------------------------------------------------------------------------------------------------
-     */
-
-    public void click_element(){
-        helpers.clickBy(select_element);
-        //driver.findElement(find_flights_button).click();
-    }
-
+package Pages.demoqa;
+public class Page_elements {
 }
--- src/main/java/pages/demoqa/Page_Index.java
+++ src/main/java/pages/demoqa/Page_Index.java
@@ -1,37 +1,58 @@
-package pages.demoqa;
+package Pages.demoqa;
 
 import Helpers.Helpers;
+import Helpers.SingletonDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class Page_Index {
+import javax.lang.model.element.Element;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Page_Index implements Page  {
+    private final String URL = Page.URL_INDEX;
+    private List<WebElement> listCategoryCards;
     public WebDriver driver;
     public Helpers helpers;
 
-    public Page_Index(WebDriver driver){
-        this.driver = driver;
+
+    public Page_Index(){
+        driver = SingletonDriver.getWebDriver();
         helpers = new Helpers(driver);
     }
 
-    /**
-     * ---------------------------------------------------------------------------------------------------------
-     *  WebElements // WebElements // WebElements // WebElements // WebElements // WebElements // WebElements //
-     * ---------------------------------------------------------------------------------------------------------
-     */
-    public By select_card = By.xpath("//div[@class=\"card mt-4 top-card\"][1]");
-
-
+    @Override
+    public String getUrl() {
+        return this.URL;
+    }
 
     /**
-     * -----------------------------------------------------------------------------------------------------------
-     *  Funciones // Funciones //  Funciones //  Funciones //  Funciones //  Funciones //  Funciones //  Funciones
-     * -----------------------------------------------------------------------------------------------------------
+     * WebElements.
      */
+    public By categoryCards = By.name("category-cards");
 
-    public void click_card(){
-        helpers.clickBy(select_card);
-        //driver.findElement(find_flights_button).click();
+    private List<WebElement> getCategoryCards(){
+        List<WebElement> categoryCards = driver.findElements(By.xpath("//div[contains(@class,'category-cards')]/div"));
+        System.out.println(categoryCards);
+        return categoryCards;
+    }
+
+    public WebElement findCategoryCard(int indexCategory){
+        if(this.listCategoryCards == null){
+            this.listCategoryCards = getCategoryCards();
+        }
+        if(!this.listCategoryCards.isEmpty()){
+            try{
+                return this.listCategoryCards.get(indexCategory);
+            }catch (IndexOutOfBoundsException e){
+                helpers.Print("Indice fuera de rango");
+            }
+        }
+        else{
+            helpers.Print("la lista de categoorias Está vacia");
+        }
+        return null;
     }
 
 }
