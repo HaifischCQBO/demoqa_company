5bf51b4364445fb80b9cb5eca206baef6d9e837d
BrayanMahecha <4684@holbertonschool.com>
2022-05-03 21:07:36 -0500

    Se agrega nuevamente page_broken por que me aparecia sin datos

--- src/main/java/pages/Page_broken.java
+++ src/main/java/pages/Page_broken.java
@@ -1,4 +1,40 @@
 package Pages;
 
+import Helpers.Helpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Page_broken {
+
+        public WebDriver driver;
+        public Helpers   helpers;
+
+        public Page_broken(WebDriver driver){
+            this.driver = driver;
+            helpers = new Helpers(driver);
+        }
 }
+
+// webElements // webElements // webElements // webElements // webElements // webElements // webElements //
+
+
+
+        private By Valid_link = By.xpath("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[2]/a[1]" );
+        private By Brok_link = By.xpath("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[2]/a[2]" );
+        private By Image_link = By.xpath("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[2]/img[1]");
+
+
+ // Funciones // // Funciones // // Funciones // // Funciones // // Funciones // // Funciones // // Funciones //
+
+
+        public void click_valid_button(){
+            helpers.double_clickBy(Valid_link);
+        }
+
+        public void click_Broken_button (){
+            helpers.double_clickBy(Brok_link);
+        }
+
+        public void click_image (){
+            helpers.double_clickBy(Image_link);
+        }
--- src/test/java/Tests/Test_POM.java
+++ src/test/java/Tests/Test_POM.java
@@ -41,7 +41,21 @@ public class Test_POM  extends BaseClass{
         Page_broken page_broken = new Page_broken(driver);
         page_broken.click_Broken_button();
 
- }   
+
+        // Nuevamente en la pagina del index seleccionamos la opcion elements
+        Page_Index page_Index = new Page_Index(driver);
+        page_Index.click_elements();
+
+
+        // En la pagina elements damos click en la opcion Broken links
+        Page_Elements page_Elements = new Page_Elements(driver);
+        Page_Elements.click_Broken_Links();
+
+        // En la pagina broken damos click en la imagen
+        Page_broken page_broken_images = new Page_broken_images(driver);
+        page_broken.click_image();
+
+    }
 
 
  @Test
