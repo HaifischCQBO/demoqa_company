e8566bd36a24c7a859e244f874743a1ded79506c
jsierrao <100698552+jsierrao@users.noreply.github.com>
2022-05-05 01:00:35 -0500

    Add files via upload

---
+++ src/test/java/Tests/Interactions/TestInteracciones.java
@@ -0,0 +1,30 @@
+package test;
+
+import baseclass.BaseClase;
+import helpers.Helpers;
+import org.testng.annotations.Test;
+import page.PageInteraccion;
+
+public class TestInteracciones extends BaseClase {
+
+
+   @Test
+    public void Testinteracciones(){
+        Helpers helpers = new Helpers(driver);
+        helpers.getURL("https://demoqa.com/");
+        helpers.max();
+
+       PageInteraccion pageInteraccion = new PageInteraccion(driver);
+       pageInteraccion.clickinteraccione();
+       pageInteraccion.espera();
+       pageInteraccion.click_selectable();
+
+
+
+
+
+
+    }
+
+    ;
+}
